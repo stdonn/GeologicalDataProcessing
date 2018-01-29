a98b9a01ddc3bb9a1d53122b3df8abf5774416ea
Stephan Donndorf <stephan.donndorf@googlemail.com>
2018-01-29 19:24:55 +0100

    Added images and make_ui.ps1 script

---
+++ make_ui.ps1
@@ -0,0 +1,5 @@
+﻿$ENV:PATH=”$ENV:PATH;C:\OSGeo4W64\bin”
+$ENV:PYTHONPATH="$ENV:PYTHONPATH;C:\OSGeo4W64\apps\Python27\Lib;C:\OSGeo4W64\apps\Python27\Lib\site-packages"
+
+pyuic4 -o .\geological_data_processing_dockwidget_base.py .\geological_data_processing_dockwidget_base.ui
+pyrcc4 -o resources_rc.py resources.qrc
